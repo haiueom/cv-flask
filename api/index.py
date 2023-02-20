071d841efd0c7fb47b21177fd24bdb8f548f959d
haiueom <ilhamtaufiq859@gmail.com>
2023-02-20 16:25:48 +0700

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template as render
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    return render(template_name_or_list="index.html")
